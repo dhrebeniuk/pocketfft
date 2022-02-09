a8662f2b69375cb6e773c77d178974d120a931c0
Dmytro Hrebeniuk <dmytrohrebeniuk@gmail.com>
2022-02-09 18:56:27 +0200

    Revert "Removed unused declarations: PocketFFTSwift"
    This reverts commit 917661deb5c9b8e49da9231677ac548d86281278.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "PocketFFT",
-            targets: ["PocketFFT"]
+            targets: ["PocketFFT", "PocketFFTSwift"]
         )
     ],
     dependencies: [],
@@ -15,6 +15,11 @@ let package = Package(
         .target(
             name: "PocketFFT",
             path: "Sources/ObjC"
+        ),
+        .target(
+            name: "PocketFFTSwift",
+            dependencies: ["PocketFFTSwift"],
+            path: "Sources/Swift"
         )
     ],
     cxxLanguageStandard: .gnucxx1z
